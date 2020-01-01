4e991fd9f175f88fc88bcf4239972791eb1412a7
jace <jacegem@gmail.com>
2020-01-02 06:22:00 +0900

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hammerspoon
+
+## Text Expander
+
+-- https://github.com/cweagans/dotfiles/commit/84da84d672bb2158b95fa28e5dd840dd21d3bb1c
+
+## ETC
+
+-- https://github.com/Hammerspoon/Spoons/blob/master/Source/KSheet.spoon/init.lua
+-- https://github.com/johngrib/hammerspoon_clipboard/tree/0.1.0
+
--- init.lua
+++ init.lua
@@ -9,14 +9,11 @@ local wifiWatcher = require('wifi')
 local visualStudioCode = require('visual_studio_code')
 
 -- text
--- https://github.com/cweagans/dotfiles/commit/84da84d672bb2158b95fa28e5dd840dd21d3bb1c
+
 ht = hs.loadSpoon("HammerText")
 ht.keywords = {
     ["$$"] = os.date("%Y-%m-%d"),
     ["..addr"] = "My address",
 }
 ht:start()
--- https://github.com/Hammerspoon/Spoons/blob/master/Source/KSheet.spoon/init.lua
--- https://github.com/johngrib/hammerspoon_clipboard/tree/0.1.0
-
 
