b494c5b3e1beabdad4622f728bf60df647456508
John <john@buzzni.com>
2020-05-15 14:10:49 +0900

    Fix

--- symbol.lua
+++ symbol.lua
@@ -2,19 +2,19 @@ local key = require('modules.key')
 
 
 key:bindUp({'shift'}, "9", function()    
-  hs.eventtap.keyStroke(nil, 'F19')  
+  -- hs.eventtap.keyStroke(nil, 'F19')  
   hs.eventtap.keyStrokes('()')
   hs.eventtap.keyStroke(nil, 'left')  
 end) 
 
 key:bindUp(nil, '[', function()
-  hs.eventtap.keyStroke(nil, 'F19')  
+  -- hs.eventtap.keyStroke(nil, 'F19')  
   hs.eventtap.keyStrokes('[]')
   hs.eventtap.keyStroke(nil, 'left')
 end)
 
 key:bindUp('shift', '[', function()
-  hs.eventtap.keyStroke(nil, 'F19')  
+  -- hs.eventtap.keyStroke(nil, 'F19')  
   hs.eventtap.keyStrokes('{}')
   hs.eventtap.keyStroke(nil, 'left')
 end)
