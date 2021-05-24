3ceb12cb5df76b98100f22f80e381e9333191fce
John <john@buzzni.com>
2021-05-24 09:17:11 +0900

    rename chrome app name

--- capslock.lua
+++ capslock.lua
@@ -9,14 +9,14 @@ local capslockShift = const.key.capslockShift
 local capslockCmdShift = const.key.capslockCmdShift
 
 -- reload
-hs.hotkey.bind(capslock, 'r', function() 
-  hs.reload()  
+hs.hotkey.bind(capslock, 'r', function()
+    hs.reload()
 end)
 hs.alert.show("HS reloaded")
 
 -- TODO: 캡스락 키 제어
-hs.hotkey.bind(capslock, 'y', function() 
-  hs.eventtap.event.newKeyEvent(nil, hs.keycodes.map.capslock, true):post()
+hs.hotkey.bind(capslock, 'y', function()
+    hs.eventtap.event.newKeyEvent(nil, hs.keycodes.map.capslock, true):post()
 end)
 
 app:launch(capslock, 'g', const.app.finder)
@@ -40,60 +40,58 @@ key:bindUp(capslock, 'x', 'cmd', 'x')
 key:bindUp(capslock, 's', 'cmd', 's')
 key:bindUp(capslock, 'f', 'cmd', 'f')
 
-
 -- 클립보드에서 앞 공백제거 후 붙여넣기
-key:bindUp(capslockShift, 'v', function() 
-  clipboard = hs.pasteboard.getContents()
-  
-  minLen = string.len(clipboard)
-  isFirst = true
-  exceptFirst = false
+key:bindUp(capslockShift, 'v', function()
+    clipboard = hs.pasteboard.getContents()
 
-  for t in clipboard:gmatch("[^\r\n]+") do
-    w, s = string.match(t, "(%s*)(.*)")
-    wLen = string.len(w)
-    sLen = string.len(s)
+    minLen = string.len(clipboard)
+    isFirst = true
+    exceptFirst = false
 
-    if sLen ~= 0 then
-      if isFirst == true then
-        isFirst = false
-        if wLen == 0 then
-          exceptFirst = true        
-        elseif wLen < minLen then
-            minLen = wLen
-        end     
-      else
-        if wLen < minLen then
-          minLen = wLen
-        end    
-      end
+    for t in clipboard:gmatch("[^\r\n]+") do
+        w, s = string.match(t, "(%s*)(.*)")
+        wLen = string.len(w)
+        sLen = string.len(s)
+
+        if sLen ~= 0 then
+            if isFirst == true then
+                isFirst = false
+                if wLen == 0 then
+                    exceptFirst = true
+                elseif wLen < minLen then
+                    minLen = wLen
+                end
+            else
+                if wLen < minLen then
+                    minLen = wLen
+                end
+            end
+        end
     end
-  end
 
-  newText = ""
-  isFirst = true
-  for t in clipboard:gmatch("[^\n]*") do  
-    if isFirst == true and exceptFirst == true then
-      newText = t .. '\n'
-      isFirst = false
-    else      
-      -- if t:match("%S") ~= nil then        
-      if string.find(t,"^%s*$") then        
-        r = t  
-      else
-        r = string.sub(t, minLen + 1, string.len(t))        
-      end
-      
-      newText = newText .. r ..'\n'
+    newText = ""
+    isFirst = true
+    for t in clipboard:gmatch("[^\n]*") do
+        if isFirst == true and exceptFirst == true then
+            newText = t .. '\n'
+            isFirst = false
+        else
+            -- if t:match("%S") ~= nil then        
+            if string.find(t, "^%s*$") then
+                r = t
+            else
+                r = string.sub(t, minLen + 1, string.len(t))
+            end
+
+            newText = newText .. r .. '\n'
+        end
     end
-  end
-  
-  result = string.sub(newText, 1, string.len(newText))
-  hs.pasteboard.setContents(result)
-  hs.eventtap.keyStroke('cmd', 'v')
-  hs.eventtap.keyStroke(nil, 'forwarddelete')
-end) 
 
+    result = string.sub(newText, 1, string.len(newText))
+    hs.pasteboard.setContents(result)
+    hs.eventtap.keyStroke('cmd', 'v')
+    hs.eventtap.keyStroke(nil, 'forwarddelete')
+end)
 
 key:bindDown(capslock, 'h', 'cmd', 'left')
 key:bindDown(capslockShift, 'h', {"cmd", "shift"}, 'left')
@@ -105,17 +103,17 @@ key:bindDown(capslock, ',', {}, 'pagedown')
 key:bindDown(capslockShift, ',', {"shift"}, 'pagedown')
 
 key:bindUp(capslock, '1', {}, 'F1', {
-  ['Evernote'] = {{'option', 'cmd'}, '1'}
+    ['Evernote'] = {{'option', 'cmd'}, '1'}
 })
 key:bindDown(capslock, '2', {}, 'F2')
 key:appleScript(capslock, '3', 'tell application "Mission Control" to launch')
 
 key:bindUp(capslock, '[', 'cmd', '[', {
-  ['Code'] = {{'ctrl'}, '-'}
+    ['Code'] = {{'ctrl'}, '-'}
 })
 
 key:bindUp(capslock, ']', 'cmd', ']', {
-  ['Code'] = {{'ctrl', 'shift'}, '-'}
+    ['Code'] = {{'ctrl', 'shift'}, '-'}
 })
 
 key:event(capslock, 'j', {}, 'left')
@@ -139,183 +137,177 @@ key:event(capslockShift, 'k', {'shift'}, 'down')
 key:event(capslockCmdShift, 'k', {'option', 'shift'}, 'down')
 
 key:event(capslock, 'u', {}, 'delete')
-key:bindUp(capslockCmd, 'u', function()   
-  hs.eventtap.keyStroke({'shift', 'option'}, 'left')
-  hs.eventtap.keyStroke(nil, 'delete')
+key:bindUp(capslockCmd, 'u', function()
+    hs.eventtap.keyStroke({'shift', 'option'}, 'left')
+    hs.eventtap.keyStroke(nil, 'delete')
 end)
 key:event(capslock, 'o', {}, 'forwarddelete')
-key:bindUp(capslockCmd, 'o', function()   
-  hs.eventtap.keyStroke({'shift', 'option'}, 'right')
-  hs.eventtap.keyStroke(nil, 'delete')
+key:bindUp(capslockCmd, 'o', function()
+    hs.eventtap.keyStroke({'shift', 'option'}, 'right')
+    hs.eventtap.keyStroke(nil, 'delete')
 end)
 
 key:event(capslock, 'q', {}, 'delete')
 key:event(capslock, 'e', {}, 'forwarddelete')
 
 function blockquoteFunc()
-  bq = "<blockquote><cite></cite></blockquote>"
-  hs.eventtap.keyStrokes(bq)
-  left(26)
+    bq = "<blockquote><cite></cite></blockquote>"
+    hs.eventtap.keyStrokes(bq)
+    left(26)
 end
 
 key:bindDown(capslock, 'b', capslock, 'b', {
-  ['Code'] = { nil, "F12"}, -- follow
-  -- ['PyCharm'] = {nil, 'F3'},  -- toggle bookmark
-  ['Android Studio'] = {nil, 'F3'},  -- toggle bookmark
+    ['Code'] = {nil, "F12"}, -- follow
+    -- ['PyCharm'] = {nil, 'F3'},  -- toggle bookmark
+    ['Android Studio'] = {nil, 'F3'} -- toggle bookmark
 })
 
-key:bindUp(capslock, 'b', capslock, 'b', {  
-  ['Code'] = { nil, "F12"},
-  ['PyCharm'] = {'cmd', 'b'},
-  ['Android Studio'] = {'cmd', 'b'},
-  ['Google Chrome'] = blockquoteFunc,
+key:bindUp(capslock, 'b', capslock, 'b', {
+    ['Code'] = {nil, "F12"},
+    ['PyCharm'] = {'cmd', 'b'},
+    ['Android Studio'] = {'cmd', 'b'},
+    ['Google Chrome'] = blockquoteFunc
 })
-
 
 function codeFunc()
-  bq = [[<code>
+    bq = [[<code>
 
 </code>]]
-  -- hs.eventtap.keyStrokes(bq)  
-  cb = hs.pasteboard.getContents()
-  hs.pasteboard.writeObjects(bq)
-  hs.eventtap.keyStroke("cmd", "v")
-  -- keyRepeat('enter', 2)
-  -- left(7)
-  keyRepeat('up', 1)
+    -- hs.eventtap.keyStrokes(bq)  
+    cb = hs.pasteboard.getContents()
+    hs.pasteboard.writeObjects(bq)
+    hs.eventtap.keyStroke("cmd", "v")
+    -- keyRepeat('enter', 2)
+    -- left(7)
+    keyRepeat('up', 1)
 
-  hs.pasteboard.writeObjects(cb)
+    hs.pasteboard.writeObjects(cb)
 
 end
 
-key:bindUp(capslock, 'd', capslock, 'd', {    
-  ['Google Chrome'] = codeFunc,
+key:bindUp(capslock, 'd', capslock, 'd', {
+    ['Google Chrome'] = codeFunc
 })
 
-key:bindDown(capslockShift, 'b', capslockShift, 'b', {  
-  ['PyCharm'] = {'cmd', 'F3'},  -- toggle bookmark
-  ['Android Studio'] = {'cmd', 'F3'},  -- toggle bookmark
+key:bindDown(capslockShift, 'b', capslockShift, 'b', {
+    ['PyCharm'] = {'cmd', 'F3'}, -- toggle bookmark
+    ['Android Studio'] = {'cmd', 'F3'} -- toggle bookmark
 })
 
-key:bindDown(capslock, 'delete', function() 
-  hs.eventtap.keyStroke('cmd', 'right')
-  hs.eventtap.keyStroke({'shift', 'cmd'}, 'left')
-  hs.eventtap.keyStroke(nil, 'delete')
-end) 
+key:bindDown(capslock, 'delete', function()
+    hs.eventtap.keyStroke('cmd', 'right')
+    hs.eventtap.keyStroke({'shift', 'cmd'}, 'left')
+    hs.eventtap.keyStroke(nil, 'delete')
+end)
 
-key:bindDown(capslock, 'space', nil, 'F13')  -- 한영전환
-
+key:bindDown(capslock, 'space', nil, 'F13') -- 한영전환
 
 -- key:bindUp(capslock, ',', capslock, ',', {
 --   ['Evernote'] = {"←"}
 -- })
 
 key:bindUp(capslock, '.', capslock, '.', {
-  ['PyCharm'] = {"option", "return"},
-  ['Android Studio'] = {"option", 'return'},
-  ['Chrome'] = {'---->'},
-  ['Code'] = {"cmd", "."},
-  ['Evernote'] = {"→"}
+    ['PyCharm'] = {"option", "return"},
+    ['Android Studio'] = {"option", 'return'},
+    ['Chrome'] = {'---->'},
+    ['Code'] = {"cmd", "."},
+    ['Evernote'] = {"→"}
 })
 
 key:bindUp(capslockShift, '.', function()
-  hs.eventtap.keyStrokes('·')
+    hs.eventtap.keyStrokes('·')
 end)
 
 key:bindDown(capslock, '/', capslock, '/', {
-  ['Google Chrome'] = {'---->>'},
+    ['Google Chrome'] = {'---->>'}
 })
 
 function dateFunc()
-  local date = os.date("%Y-%m-%d") 
-  hs.eventtap.keyStrokes(date)
+    local date = os.date("%Y-%m-%d")
+    hs.eventtap.keyStrokes(date)
 end
 
 function h3dateFunc()
-  local date = os.date("%Y-%m-%d") 
-  hs.eventtap.keyStrokes("### " .. date)
+    local date = os.date("%Y-%m-%d")
+    hs.eventtap.keyStrokes("### " .. date)
 end
 
 function dateFuncShort()
-  weekNames = { "일", "월", "화", "수", "목", "금", "토" }
-  cNow = os.date("*t")
-  wday = weekNames[cNow["wday"]]
-  local date = os.date("%y년 %m월 %d일")
-  date = "#### " .. date .. " " .. "(" .. wday .. ")"
-  hs.eventtap.keyStrokes(date)
+    weekNames = {"일", "월", "화", "수", "목", "금", "토"}
+    cNow = os.date("*t")
+    wday = weekNames[cNow["wday"]]
+    local date = os.date("%y년 %m월 %d일")
+    date = "#### " .. date .. " " .. "(" .. wday .. ")"
+    hs.eventtap.keyStrokes(date)
 end
 
-
 function keyRepeat(key, times)
-  for i=1, times, 1 do
-    hs.eventtap.event.newKeyEvent(nil, key, true):post()
-    hs.eventtap.event.newKeyEvent(nil, key, false):post()
-  end
+    for i = 1, times, 1 do
+        hs.eventtap.event.newKeyEvent(nil, key, true):post()
+        hs.eventtap.event.newKeyEvent(nil, key, false):post()
+    end
 end
 
 function left(times)
-  for i=1, times, 1 do
-    hs.eventtap.event.newKeyEvent(nil, "left", true):post()
-    hs.eventtap.event.newKeyEvent(nil, "left", false):post()
-  end
+    for i = 1, times, 1 do
+        hs.eventtap.event.newKeyEvent(nil, "left", true):post()
+        hs.eventtap.event.newKeyEvent(nil, "left", false):post()
+    end
 end
 
 function todoFunc()
-  now=os.time()
-  days = 7  
-  numberOfDays = now + days * 24 * 3600
-  dateAfterNumberOfDays = os.date("%Y-%m-%d",numberOfDays)
-  todo = "<todo due:" .. dateAfterNumberOfDays .. "></todo>"
-  hs.eventtap.keyStrokes(todo)
-  left(7)
+    now = os.time()
+    days = 7
+    numberOfDays = now + days * 24 * 3600
+    dateAfterNumberOfDays = os.date("%Y-%m-%d", numberOfDays)
+    todo = "<todo due:" .. dateAfterNumberOfDays .. "></todo>"
+    hs.eventtap.keyStrokes(todo)
+    left(7)
 end
 
-
 key:bindDown(capslock, '2', capslock, '2', {
-  ['Code'] = {nil, 'F2'},
-  ['PyCharm'] = {'shift', 'F6'},  
-  ['Google Chrome'] = dateFuncShort,  
-  ['Notion'] = dateFunc, 
-  ['XD'] = dateFunc,  
+    ['Code'] = {nil, 'F2'},
+    ['PyCharm'] = {'shift', 'F6'},
+    ['Google Chrome'] = dateFuncShort,
+    ['Notion'] = dateFunc,
+    ['XD'] = dateFunc
 })
 
-key:bindDown(capslock, '3', capslock, '3', {  
-  ['PyCharm'] = {nil, 'F2'},  
-  ['Android Studio'] = {nil, 'F2'}, 
-  ['Google Chrome'] = todoFunc,   
-  ['Notion'] = h3dateFunc, 
+key:bindDown(capslock, '3', capslock, '3', {
+    ['PyCharm'] = {nil, 'F2'},
+    ['Android Studio'] = {nil, 'F2'},
+    ['Google Chrome'] = todoFunc,
+    ['Notion'] = h3dateFunc
 })
 
 key:bindDown(capslock, '4', capslock, '4', {
-  ['Code'] = { '$$ $$' },
+    ['Code'] = {'$$ $$'}
 })
 
-key:bindDown(capslock, '6', capslock, '6', {  
-  ['Android Studio'] = {'shift', 'F6'},
-  ['PyCharm'] = {'shift', 'F6'},
+key:bindDown(capslock, '6', capslock, '6', {
+    ['Android Studio'] = {'shift', 'F6'},
+    ['PyCharm'] = {'shift', 'F6'}
 })
 
-key:bindDown(capslock, '9', capslock, '9', {  
-  ['Chrome'] = {'←'},  
+key:bindDown(capslock, '9', capslock, '9', {
+    ['Chrome'] = {'←'}
 })
 
 key:bindDown(capslock, '0', capslock, '0', {
-  ['PyCharm'] = {'cmd', 'F3'},  -- show bookmark list
-  ['Chrome'] = {'→'},
+    ['PyCharm'] = {'cmd', 'F3'}, -- show bookmark list
+    ['Chrome'] = {'→'}
 })
 
-
-key:bindDown(capslock, '-', capslock, '-', {
-  ['PyCharm'] = {'shift', 'F2'},  -- show bookmark list
-  ['Chrome'] = {{'ctrl', 'shift'}, 'tab'},
+key:bindUp(capslock, '-', capslock, '-', {
+    ['PyCharm'] = {'shift', 'F2'}, -- show bookmark list
+    [const.app.chrome] = {{'ctrl', 'shift'}, 'tab'}
 })
 
-key:bindDown(capslock, '=', capslock, '=', {
-  ['PyCharm'] = {nil, 'F2'},  -- show bookmark list
-  ['Chrome'] = {{'ctrl'}, 'tab'},
+key:bindUp(capslock, '=', capslock, '=', {
+    ['PyCharm'] = {nil, 'F2'}, -- show bookmark list
+    [const.app.chrome] = {'ctrl', 'tab'}
 })
 
-    
 hs.hotkey.bind(capslock, "Left", window:move("left"))
 hs.hotkey.bind(capslock, "Right", window:move("right"))
 hs.hotkey.bind(capslock, "Up", window:move("up"))
--- function.lua
+++ function.lua
@@ -1,46 +1,44 @@
-
 local const = require('modules.const')
 local key = require('modules.key')
 
-local capslock= const.key.capslock
+local capslock = const.key.capslock
 local capslockShift = const.key.capslockShift
 
 function volumeChange(isIncrement)
-  local volume = hs.audiodevice.current().volume  
-  -- When the volume gets near zero, change it in smaller increments. Otherwise even the first increment
-  -- above zero may be too loud.
-  -- NOTE(phil): I noticed that using a decimal smaller than 0.4 will sometimes result in the volume remaining
-  -- unchanged after calling setVolume, as if OSX only lets you change the volume by large increments.
-  local diff = 2
+    local volume = hs.audiodevice.current().volume
+    -- When the volume gets near zero, change it in smaller increments. Otherwise even the first increment
+    -- above zero may be too loud.
+    -- NOTE(phil): I noticed that using a decimal smaller than 0.4 will sometimes result in the volume remaining
+    -- unchanged after calling setVolume, as if OSX only lets you change the volume by large increments.
+    local diff = 2
 
-  if volume < 2 then 
-    diff = 0.4
-  elseif volume > 50 then
-    diff = 16
-  elseif volume > 10 then
-    diff = 4  
-  end
-    
-  if isIncrement == true then
-    newVolume = volume + diff
-  else
-    newVolume = volume - diff
-  end
+    if volume < 2 then
+        diff = 0.4
+    elseif volume > 50 then
+        diff = 16
+    elseif volume > 10 then
+        diff = 4
+    end
 
-  if newVolume > 100 then
-    newVolume = 100
-  elseif newVolume < 0 then
-    newVolume = 0
-  end
+    if isIncrement == true then
+        newVolume = volume + diff
+    else
+        newVolume = volume - diff
+    end
 
-  hs.alert.show(tonumber(string.format("%.1f", newVolume)))
-  hs.audiodevice.defaultOutputDevice():setVolume(newVolume)
+    if newVolume > 100 then
+        newVolume = 100
+    elseif newVolume < 0 then
+        newVolume = 0
+    end
+
+    hs.alert.show(tonumber(string.format("%.1f", newVolume)))
+    hs.audiodevice.defaultOutputDevice():setVolume(newVolume)
 end
 
 -----------------------------------------
 -- Defalut function keys
 -----------------------------------------
-
 
 -- key:bindDown(nil, 'F2', nil, 'F2', {
 --   ['PyCharm'] = {'shift', 'F6'},
@@ -52,40 +50,38 @@ end
 -- })
 
 key:bindDown(nil, 'F4', nil, 'F4', {
-  ['Chrome'] = {nil, 'pagedown'},
-  ['Code'] = {nil, 'F12'},
-  ['PyCharm'] = {'cmd', 'b'},
-  ['Android Studio'] = {'cmd', 'b'},
+    ['Chrome'] = {nil, 'pagedown'},
+    ['Code'] = {nil, 'F12'},
+    ['PyCharm'] = {'cmd', 'b'},
+    ['Android Studio'] = {'cmd', 'b'}
 })
 
 key:bindDown(nil, 'F5', nil, 'F5', {
-  ['PyCharm'] = {'ctrl', 'd'}, -- debug run
-  ['Google Chrome'] = {'cmd', 'r'},
-  ['Code'] = {nil, 'F5'},
+    ['PyCharm'] = {'ctrl', 'd'}, -- debug run
+    ['Google Chrome'] = {'cmd', 'r'},
+    ['Code'] = {nil, 'F5'}
 })
 
 key:bindDown(nil, 'F6', nil, 'F6', {
-  ['PyCharm'] = {'ctrl', 'r'}, -- run
+    ['PyCharm'] = {'ctrl', 'r'} -- run
 })
 
 key:bindDown(nil, 'F9', nil, 'F9', {
-  ['PyCharm'] = {{'option', 'cmd'}, 'r'},
-  ['Android Studio'] = {{'option', 'cmd'}, 'r'},
+    ['PyCharm'] = {{'option', 'cmd'}, 'r'},
+    ['Android Studio'] = {{'option', 'cmd'}, 'r'}
 })
-
 
 -----------------------------------------
 -- Capslock function keys
 -----------------------------------------
 key:bindDown(capslock, 'F6', capslock, 'F6', {
-  ['PyCharm'] = {'shift', 'F6'},
-  ['Android Studio'] = {'shift', 'F6'},
+    ['PyCharm'] = {'shift', 'F6'},
+    ['Android Studio'] = {'shift', 'F6'}
 })
 
 key:bindDown(capslock, "F9", capslock, 'F9', {
-  ['Android Studio'] = {{'option', 'cmd'}, 'r'},
+    ['Android Studio'] = {{'option', 'cmd'}, 'r'}
 })
-
 
 -- hs.hotkey.bind('cmd', "F10", nil, function()  
 --   for i=1, 10 do -- 기본 증가치는 1이 된다
@@ -95,16 +91,16 @@ key:bindDown(capslock, "F9", capslock, 'F9', {
 --   end 
 -- end)
 
-hs.hotkey.bind(capslock, "F10", function()  
-  hs.audiodevice.defaultOutputDevice():setVolume(0)
+hs.hotkey.bind(capslock, "F10", function()
+    hs.audiodevice.defaultOutputDevice():setVolume(0)
 end)
 
 hs.hotkey.bind(capslock, "F11", function()
-  volumeChange(false)
+    volumeChange(false)
 end)
 
 hs.hotkey.bind(capslock, "F12", function()
-  volumeChange(true)
+    volumeChange(true)
 end)
 
 key:bindDown(nil, 'end', 'cmd', 'right', {})
@@ -112,17 +108,19 @@ key:bindDown('shift', 'end', {'cmd', 'shift'}, 'right', {})
 key:bindDown(nil, 'home', 'cmd', 'left', {})
 key:bindDown('shift', 'home', {'cmd', 'shift'}, 'left', {})
 
-
-view = hs.webview.new({x = 10, y = 10, w = 150, h = 100})
+view = hs.webview.new({
+    x = 10,
+    y = 10,
+    w = 150,
+    h = 100
+})
 view:windowStyle("closable")
 view:windowStyle("titled")
 view:windowStyle("resizable")
 view:url('file:///' .. os.getenv("HOME") .. '/test/menu.html')
 
 hs.hotkey.bind("shift", "f4", function()
-  local email = 'test@email.com'
-  hs.messages.SMS(email, "Test iMessage")
+    local email = 'test@email.com'
+    hs.messages.SMS(email, "Test iMessage")
 end)
-
-
 
