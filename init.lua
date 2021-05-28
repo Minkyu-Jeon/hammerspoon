3c4857daa7602ee9f2e35da7d95dd4350fbc41fa
John <john@buzzni.com>
2021-05-28 09:28:04 +0900

    formatting

--- init.lua
+++ init.lua
@@ -1,9 +1,7 @@
-
 local const = require('modules.const')
-local capslock= const.key.capslock
+local capslock = const.key.capslock
 local capslockCmd = const.key.capslockCmd
 local capslockShift = const.key.capslockShift
-
 
 require('modules.inputsource_aurora')
 require('modules.caffein'):init(capslockShift, 'p')
@@ -19,14 +17,14 @@ require('altMode')
 -- Spoons
 local ClipboardTool = hs.loadSpoon("ClipboardTool")
 ClipboardTool:start()
-hs.hotkey.bind(capslock, 'p', function() 
-  ClipboardTool:toggleClipboard()
+hs.hotkey.bind(capslock, 'p', function()
+    ClipboardTool:toggleClipboard()
 end)
 
 local aclock = hs.loadSpoon('AClock')
 hs.hotkey.bind(capslockShift, "0", function()
-  aclock:toggleShow()
-end)          
+    aclock:toggleShow()
+end)
 
 -- hs.hotkey.bind(capslock, '\'', function()
 --   expose = hs.expose.new(nil,{showThumbnails=true, includeOtherSpaces=true}) 
@@ -34,34 +32,34 @@ end)
 -- end)
 
 -- Make the alerts look nicer.
-hs.alert.defaultStyle.strokeColor =  {white = 1, alpha = 0}
-hs.alert.defaultStyle.fillColor =  {white = 0.05, alpha = 0.55}
-hs.alert.defaultStyle.radius =  10
-
-
+hs.alert.defaultStyle.strokeColor = {
+    white = 1,
+    alpha = 0
+}
+hs.alert.defaultStyle.fillColor = {
+    white = 0.05,
+    alpha = 0.55
+}
+hs.alert.defaultStyle.radius = 10
 
 function applicationWatcher(appName, eventType, appObject)
-  if (eventType == hs.application.watcher.activated) then
-      if (appName == "Finder") then
-        print(appName)
-          -- Bring all Finder windows forward when one gets activated
-          -- appObject:selectMenuItem({"Window", "Bring All to Front"})
-          appObject:selectMenuItem({"윈도우", "모두 앞으로 가져오기"})
-      end
-  end
+    if (eventType == hs.application.watcher.activated) then
+        if (appName == "Finder") then
+            print(appName)
+            -- Bring all Finder windows forward when one gets activated
+            -- appObject:selectMenuItem({"Window", "Bring All to Front"})
+            appObject:selectMenuItem({"윈도우", "모두 앞으로 가져오기"})
+        end
+    end
 end
 appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
 
 -- Make the console dark
---hs.console.darkMode(false)
-
+-- hs.console.darkMode(false)
 
 -- hs.loadSpoon("ModalMgr")
 -- hs.loadSpoon("AClock")
-
-
-
 
 -- Register AClock
 -- if spoon.AClock then
--- text.lua
+++ text.lua
@@ -1,142 +1,165 @@
 local date = require('modules.date')
 
 function left(times)
-  for i=1, times, 1 do
-    hs.eventtap.event.newKeyEvent(nil, "left", true):post()
-    hs.eventtap.event.newKeyEvent(nil, "left", false):post()
-  end
+    for i = 1, times, 1 do
+        hs.eventtap.event.newKeyEvent(nil, "left", true):post()
+        hs.eventtap.event.newKeyEvent(nil, "left", false):post()
+    end
 end
 
 function todo(days)
-  dateAfterNumberOfDays = date:formatAddDays(days)
-  local text = "<todo due:" .. dateAfterNumberOfDays .. "></todo>"
-  if days == 0 then
-    text = "<todo></todo>"
-  end
-  hs.eventtap.keyStrokes(text)
-  left(7)
+    dateAfterNumberOfDays = date:formatAddDays(days)
+    local text = "<todo due:" .. dateAfterNumberOfDays .. "></todo>"
+    if days == 0 then
+        text = "<todo></todo>"
+    end
+    hs.eventtap.keyStrokes(text)
+    left(7)
 end
 
-function code(lang)  
-  local text = "```" .. lang .. "\n\n```"
-  hs.pasteboard.setContents(text)  
-  hs.eventtap.keyStroke('cmd', 'v')
-  -- hs.eventtap.keyStroke({}, 'up')
-  -- hs.eventtap.keyStroke({}, 'up')
+function code(lang)
+    local text = "```" .. lang .. "\n\n```"
+    hs.pasteboard.setContents(text)
+    hs.eventtap.keyStroke('cmd', 'v')
+    -- hs.eventtap.keyStroke({}, 'up')
+    -- hs.eventtap.keyStroke({}, 'up')
 end
-
 
 function str(type)
-  -- 마지막 글자가 자음 하나이면, 따로 처리한다.
-  local len = string.len(type)
-  local word = string.sub(type, 0, len-3)
-  local last = string.sub(type, len-2, len)
+    -- 마지막 글자가 자음 하나이면, 따로 처리한다.
+    local len = string.len(type)
+    local word = string.sub(type, 0, len - 3)
+    local last = string.sub(type, len - 2, len)
 
-  hs.eventtap.keyStrokes(word)
+    hs.eventtap.keyStrokes(word)
 
-  if last == 'ㄷ' then 
-    hs.eventtap.keyStroke(nil, 'e')
-  elseif last == 'ㄴ' then 
-    hs.eventtap.keyStroke(nil, 's')
-  elseif last == 'ㅇ' then 
-      hs.eventtap.keyStroke(nil, 'd')
-  else
-    hs.eventtap.keyStrokes(last)
-  end
+    if last == 'ㄷ' then
+        hs.eventtap.keyStroke(nil, 'e')
+    elseif last == 'ㄴ' then
+        hs.eventtap.keyStroke(nil, 's')
+    elseif last == 'ㅇ' then
+        hs.eventtap.keyStroke(nil, 'd')
+    else
+        hs.eventtap.keyStrokes(last)
+    end
 end
 
 function change()
-  hs.eventtap.keyStroke(nil, 'F13') 
+    hs.eventtap.keyStroke(nil, 'F13')
 end
-
 
 -- text expander
 ht = hs.loadSpoon("HammerText")
 ht.keywords = {
-    ["$$"] = { os.date("%Y-%m-%d") },
-    ["@@"] = { "$$" },
+    ["$$"] = {os.date("%Y-%m-%d")},
+    ["@@"] = {"$$"},
     ["..addr"] = "My address",
-    [" ne "] = { " != " },
-    [";."] = { "·" },
-    [";1"] = { "I" },
-    [";2"] = { "II" },
-    [";3"] = { "III" },
-    [";4"] = { "IV" },
-    [";5"] = { "Ⅴ" },
-    [";6"] = { "VI" },
-    [";7"] = { "VII" },
-    [";8"] = { "VIII" },
-    [";9"] = { "IX" },
-    [";10"] = { "X" },    
-    ["1["] = { nil, function()     
-        local clip = hs.pasteboard.getContents()    
+    [" ne "] = {" != "},
+    [";."] = {"·"},
+    [";1"] = {"I"},
+    [";2"] = {"II"},
+    [";3"] = {"III"},
+    [";4"] = {"IV"},
+    [";5"] = {"Ⅴ"},
+    [";6"] = {"VI"},
+    [";7"] = {"VII"},
+    [";8"] = {"VIII"},
+    [";9"] = {"IX"},
+    [";10"] = {"X"},
+    ["1["] = {nil, function()
+        local clip = hs.pasteboard.getContents()
         local text = "![](" .. clip .. ")"
         hs.eventtap.keyStrokes(text)
-      end
-    },
-    ["ㅎㅏㅁㄱㅔ"] = { "함께", nil, 3 },
-    ["ㄴㅡㄱㅣ"] = { "느끼", nil, 3 },    
-    ['/tt'] = { nil, function() todo(0) end },
-    ['/tot'] = { nil, function() todo(1) end },
-    ['/tow'] = { nil, function() todo(7) end },
-    ['/tom'] = { nil, function() todo(30) end },
-    ['/fb'] = { nil, function()
-      local text = "<fragment-block></fragment-block>"
-      hs.eventtap.keyStrokes(text)
-      left(17)      
-      end 
-    },    
-    ['`js'] = { nil, function() code('javascript') end },
-    ['`ㅓㄴ'] = { nil, function() code('javascript') end },
-    ['`py'] = { nil, function() code('python') end },
-    ['`ㅔㅛ'] = { nil, function() code('python') end },
-    ['`sh'] = { nil, function() code('shell') end },
-    ['`ㄴㅗ'] = { nil, function() code('shell') end },
-    ['`sq'] = { nil, function() code('sql') end },
-    ['`ㄴㅂ'] = { nil, function() code('sql') end },
+    end},
+    ["ㅎㅏㅁㄱㅔ"] = {"함께", nil, 3},
+    ["ㄴㅡㄱㅣ"] = {"느끼", nil, 3},
+    ['/tt'] = {nil, function()
+        todo(0)
+    end},
+    ['/tot'] = {nil, function()
+        todo(1)
+    end},
+    ['/tow'] = {nil, function()
+        todo(7)
+    end},
+    ['/tom'] = {nil, function()
+        todo(30)
+    end},
+    ['/fb'] = {nil, function()
+        local text = "<fragment-block></fragment-block>"
+        hs.eventtap.keyStrokes(text)
+        left(17)
+    end},
+    ['`js'] = {nil, function()
+        code('javascript')
+    end},
+    ['`ㅓㄴ'] = {nil, function()
+        code('javascript')
+    end},
+    ['`py'] = {nil, function()
+        code('python')
+    end},
+    ['`ㅔㅛ'] = {nil, function()
+        code('python')
+    end},
+    ['`sh'] = {nil, function()
+        code('shell')
+    end},
+    ['`ㄴㅗ'] = {nil, function()
+        code('shell')
+    end},
+    ['`sq'] = {nil, function()
+        code('sql')
+    end},
+    ['`ㄴㅂ'] = {nil, function()
+        code('sql')
+    end},
     -- 한글
     ['ㄱㅔㅅㄷ'] = {'겠ㄷ', nil, 2},
-    ['ㅇㅣㅅㄴ'] = { nil, function() str('있ㄴ') end, 2},
-    ['ㅇㅣㅅㅅ'] = { nil, function() str('있ㅅ') end, 2},
-    ['ㅇㅣㅅㅇ'] = { nil, function() str('있ㅇ') end, 2},
-    ['ㅇㅣㅅㄷ'] = { nil, function() str('있ㄷ') end, 2},
+    ['ㅇㅣㅅㄴ'] = {nil, function()
+        str('있ㄴ')
+    end, 2},
+    ['ㅇㅣㅅㅅ'] = {nil, function()
+        str('있ㅅ')
+    end, 2},
+    ['ㅇㅣㅅㅇ'] = {nil, function()
+        str('있ㅇ')
+    end, 2},
+    ['ㅇㅣㅅㄷ'] = {nil, function()
+        str('있ㄷ')
+    end, 2},
     ['ㅇㅗㅏㅅㄷ'] = {'왔ㄷ', nil, 2},
     -- 한 -> 영
-    ['ㅐㅏㄱ'] = { 'OKR', nil, 3 },
-    ['ㅁㅔㅑ'] = { 'API', nil, 2 },
+    ['ㅐㅏㄱ'] = {'OKR', nil, 3},
+    ['ㅁㅔㅑ'] = {'API', nil, 2},
     -- ['ㅕㄱㅣ'] = { 'URL', nil, 3 },
     -- Symbol
-    ['--='] = { '→' },
-    ['0--'] = { '←' },
-    ['0-='] = { '↔️' },
-    ['#3'] = { '###' },
-    ['#4'] = { '####' },
+    ['--='] = {'→'},
+    ['0--'] = {'←'},
+    ['0-='] = {'↔️'},
+    ['#3'] = {'###'},
+    ['#4'] = {'####'},
     -- Alphabet    
-    [';ㅁ'] = { 'A' },
-    [';ㅠ'] = { 'B' },
-    [';ㅇ'] = { 'D' },
-    [';ㅎ'] = { 'G' },
-    [';ㅌ'] = { 'X' },
+    [';ㅁ'] = {'A'},
+    [';ㅠ'] = {'B'},
+    [';ㅇ'] = {'D'},
+    [';ㅎ'] = {'G'},
+    [';ㅌ'] = {'X'},
     -- esperanto
-    ['Cx'] = { 'Ĉ'},
-    ['cx'] = { 'ĉ'},
-    ['Gx'] = { 'Ĝ'},
-    ['gx'] = { 'ĝ'},
-    ['Hx'] = { 'Ĥ'},
-    ['hx'] = { 'ĥ'},
-    ['Jx'] = { 'Ĵ'},
-    ['jx'] = { 'ĵ'},
-    ['Sx'] = { 'Ŝ'},
-    ['sx'] = { 'ŝ'},
-    ['Ux'] = { 'Ŭ'},
-    ['ux'] = { 'ŭ'},
+    ['Cx'] = {'Ĉ'},
+    ['cx'] = {'ĉ'},
+    ['Gx'] = {'Ĝ'},
+    ['gx'] = {'ĝ'},
+    ['Hx'] = {'Ĥ'},
+    ['hx'] = {'ĥ'},
+    ['Jx'] = {'Ĵ'},
+    ['jx'] = {'ĵ'},
+    ['Sx'] = {'Ŝ'},
+    ['sx'] = {'ŝ'},
+    ['Ux'] = {'Ŭ'},
+    ['ux'] = {'ŭ'},
     -- notion
-    ['/ㅅㅁ'] = { '/table' },
+    ['/ㅅㅁ'] = {'/table'}
 }
 
-
 ht:start()
-
-
-
-
