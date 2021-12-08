c9140df5632756ae705265c4211b6afcacc367be
Jace <jacegem@gmail.com>
2021-12-08 14:04:18 +0900

    fix symbol

--- symbol.lua
+++ symbol.lua
@@ -1,20 +1,15 @@
 local key = require('modules.key')
+local inputEnglish = "com.apple.keylayout.ABC"
 
 key:bindUp({'shift'}, "9", function()
-    hs.eventtap.keyStroke(nil, 'F14')
+    local inputSource = hs.keycodes.currentSourceID()
+    if not (inputSource == inputEnglish) then
+        hs.eventtap.keyStroke(nil, 'F14')
+    end
+
     hs.eventtap.keyStrokes('()')
-    -- hs.eventtap.keyStroke(nil, 'left')
-    -- hs.eventtap.keyStroke(nil, 'right')
-    -- hs.eventtap.keyStrokes('(')
-    -- hs.eventtap.keyStrokes(')')
     hs.eventtap.keyStroke(nil, 'left')
 end)
-
--- key:bindUp(nil, '[', function()
---   -- hs.eventtap.keyStroke(nil, 'F19')  
---   hs.eventtap.keyStrokes('[]')
---   hs.eventtap.keyStroke(nil, 'left')
--- end)
 
 function symbolDouble()
     hs.eventtap.keyStrokes('[]')
@@ -32,3 +27,14 @@ key:bindUp('shift', '[', function()
     hs.eventtap.keyStrokes('{}')
     hs.eventtap.keyStroke(nil, 'left')
 end)
+
+-- hs.eventtap.keyStroke(nil, 'left')
+-- hs.eventtap.keyStroke(nil, 'right')
+-- hs.eventtap.keyStrokes('(')
+-- hs.eventtap.keyStrokes(')')
+
+-- key:bindUp(nil, '[', function()
+--   -- hs.eventtap.keyStroke(nil, 'F19')  
+--   hs.eventtap.keyStrokes('[]')
+--   hs.eventtap.keyStroke(nil, 'left')
+-- end)
