6401b637767f21f6d8c8ab69d0231adb23422bc7
jace <jacegem@gmail.com>
2022-01-19 01:43:43 +0900

    Add commnad

--- command.lua
+++ command.lua
@@ -5,7 +5,8 @@ hs.hotkey.bind({'cmd'}, 'f13', function()
 end)
 
 key:bindUp('cmd', 'return', 'cmd', 'return', {
-    ['Chrome'] = {'ctrl', 'return'} -- Save dokuwiki
+    ['Chrome'] = {'ctrl', 'return'}, -- Save dokuwiki
+    ['IntelliJ IDEA'] = {{'shift', 'cmd'}, 'p'}
 })
 
 key:bindUp({'shift', 'cmd'}, 'a', {'shift', 'cmd'}, 'a', {
@@ -13,8 +14,17 @@ key:bindUp({'shift', 'cmd'}, 'a', {'shift', 'cmd'}, 'a', {
 })
 
 key:bindUp({'cmd'}, 'e', {'cmd'}, 'e', {
-    ['Code'] = {{'shift', 'cmd'}, 'e'} -- Change Dark Theme Shortcut
+    ['Code'] = {{'shift', 'cmd'}, 'e'}, -- Change Dark Theme Shortcut
+    ['IntelliJ IDEA'] = {{'shift', 'cmd'}, 'o'}
 })
 
 
+-- intellij Cursive
 
+key:bindDown({'shift', 'cmd'}, 'j', {'shift', 'cmd'}, 'j', {
+    -- ['IntelliJ IDEA'] = {{'shift', 'cmd'},'j'} 
+})
+
+key:bindUp({'shift', 'cmd'}, 'l', {'shift', 'cmd'}, 'l', {
+    ['IntelliJ IDEA'] = {{'shift', 'cmd'}, 'k'} 
+})
